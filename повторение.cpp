6e62e2420d4c12a030319b67b20639527e636917
Kalitka7 <krupskaam5@gmail.com>
2024-05-28 21:25:30 +0700

    Add files via upload

---
+++ повторение.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+
+std::vector<int> increaseVector(std::vector<int> vec, int n) {
+    for (int i = 0; i < vec.size(); i++) {
+        vec[i] += n;
+    }
+    return vec;
+}
+#include <vector>
+#include "yourFileWithFunction.cpp" // Подключаем файл с функцией
+
+void testIncreaseVector() {
+    std::vector<int> input = { 1, 2, 3, 4, 5 };
+    int n = 10;
+
+    std::vector<int> expected = { 11, 12, 13, 14, 15 };
+    std::vector<int> result = increaseVector(input, n);
+
+    assert(result == expected);
+}
+
+int main() {
+    testIncreaseVector();
+    return 0;
+}
